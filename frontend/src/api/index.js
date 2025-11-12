925c2e370d2f22422bc42cde3249a81880a30825
Polaki Sai Kiran <121670156+KiranPolaki@users.noreply.github.com>
2025-11-12 16:29:32 +0530

    update the path to prod

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import { useAuthStore } from "../store/authStore";
 
 const apiClient = axios.create({
-  // baseURL: "https://budgettracker-sbwg.onrender.com/api",
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: "https://budgettracker-sbwg.onrender.com/api",
+  // baseURL: "http://127.0.0.1:8000/api",
   headers: {
     "Content-Type": "application/json",
   },
